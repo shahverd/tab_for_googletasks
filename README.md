567633cc18aab19f199e4e74b5dabb7a2e1f0abb
Mostafa Shahverdy <shahverdy.m@gmail.com>
2023-03-29 23:51:03 +0330

    README

---
+++ README.md
@@ -0,0 +1,11 @@
+What Does it provide?
+=====================
+
+Nothing!
+It just makes it easy to get to your Google Tasks without having to open Gmail. Just click the extension icon in your toolbar and your Tasks will open in a new tab.
+
+With this extension, you can:
+
+ - Stay organized and productive by keeping your Tasks separate from your Gmail.
+ - Never miss a Task by having your Tasks open in a new tab that you can easily access.
+ - Be more productive by being able to focus on your Tasks without being distracted by your Gmail.
--- content.js
+++ content.js
@@ -1,23 +1,24 @@
 setTimeout(function () {
 
-  if (!document.getElementById('banner')) {
-    var banner = document.createElement('div');
-    banner.id = 'banner';
-    banner.style.textAlign = 'center';
-    
-    var h2 = document.createElement('h2');
-    h2.innerText = 'Google Tasks';
-    h2.style = "color:grey; font-weight: 500;  font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif";
-    banner.appendChild(h2);
-    
-    var img = document.createElement('img');
-    img.src = "https://upload.wikimedia.org/wikipedia/commons/5/5b/Google_Tasks_2021.svg";
-    img.style = "max-width: 25px; margin-right: 5px; margin-bottom: -2px"
-    h2.prepend(img);
+    if (!document.getElementById('banner'))
+    {
+        var banner = document.createElement('div');
+        banner.id = 'banner';
+        banner.style.textAlign = 'center';
 
-    var parentElement = document.querySelector('c-wiz>div');
-    var referenceElement = document.querySelector('c-wiz>div>div');
-    referenceElement.style.height = 'calc(100% - 100px)';
-    parentElement.insertBefore(banner, referenceElement);
-  }
+        var h2 = document.createElement('h2');
+        h2.innerText = 'Google Tasks';
+        h2.style = "color:grey; font-weight: 500;  font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif";
+        banner.appendChild(h2);
+
+        var img = document.createElement('img');
+        img.src = "https://upload.wikimedia.org/wikipedia/commons/5/5b/Google_Tasks_2021.svg";
+        img.style = "max-width: 25px; margin-right: 5px; margin-bottom: -2px"
+        h2.prepend(img);
+
+        var parentElement = document.querySelector('c-wiz>div');
+        var referenceElement = document.querySelector('c-wiz>div>div');
+        referenceElement.style.height = 'calc(100% - 100px)';
+        parentElement.insertBefore(banner, referenceElement);
+    }
 }, 500);
